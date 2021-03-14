8ddd0367f5f80c9987a33dc1424b14281b397283
Stan van der Bend <stanvdbend@gmail.com>
2021-03-14 17:25:52 +0100

    Defined repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
     id("org.openjfx.javafxplugin") version("0.0.9")
 }
 
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
 dependencies {
     implementation( "org.slf4j:slf4j-api:1.7.30")
     implementation("org.openjfx:javafx-web:11.0.2")
